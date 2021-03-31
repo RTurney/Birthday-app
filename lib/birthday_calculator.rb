01dc656cd42336d9ad194dfdc2f84814162c013a
RTurney <r.turney93@gmail.com>
2021-03-31 14:36:27 +0100

    Refactor BirthdayCalculator class

--- lib/birthday_calculator.rb
+++ lib/birthday_calculator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-#Birthday calculator class
-class Birthday_calculator
+# Birthday calculator class
+class BirthdayCalculator
   def initialize
     @today = Date.today
   end
@@ -9,21 +9,24 @@ class Birthday_calculator
   def calculate_days(day, month)
     birthday = birthday_converter(day, month)
 
-    return 0 if is_today?(birthday)
+    return 0 if today?(birthday)
 
-     if Date.today >= birthday
-       birthday = Date.new((Date.today.year + 1), month.to_i, day.to_i)
-     end
-    return (birthday - @today).to_i
+    birthday = add_year(day, month) if Date.today >= birthday
+
+    (birthday - @today).to_i
   end
 
-
   private #----------------------------
-  def is_today?(birthday)
+
+  def today?(birthday)
     birthday == @today
   end
 
   def birthday_converter(day, month)
     Date.new(Date.today.year, month.to_i, day.to_i)
+  end
+
+  def add_year(day, month)
+    Date.new((Date.today.year + 1), month.to_i, day.to_i)
   end
 end
--- spec/birthday_calculator_spec.rb
+++ spec/birthday_calculator_spec.rb
@@ -2,16 +2,16 @@
 
 require 'birthday_calculator'
 
-describe Birthday_calculator do
+describe BirthdayCalculator do
   it 'will return 0 if birthday is today' do
     expect(subject.calculate_days('31', '03')).to eq 0
   end
 
-  it "will return 1 if birthday is tomorrow" do
+  it 'will return 1 if birthday is tomorrow' do
     expect(subject.calculate_days('01', '04')).to eq 1
   end
 
-  it "will return 364 if birthday was yesterday" do
+  it 'will return 364 if birthday was yesterday' do
     expect(subject.calculate_days('30', '03')).to eq 364
   end
 end
