85562c7ac5dde67ad495d02662fa6ebd8a45b156
RTurney <r.turney93@gmail.com>
2021-03-31 14:12:41 +0100

    Convert birthday calculator into class

--- lib/birthday_calculator.rb
+++ lib/birthday_calculator.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
-def birthday_calculator(birthday)
-  bday = birthday.split('/').join().to_i
-  today = Time.new.strftime("%d/%m").split('/').join().to_i
-  if bday == today
-    return 0
+class Birthday_calculator
+
+  def initialize
+    @today = Time.new.strftime("%d/%m").split('/').join().to_i
   end
-  
+
+  def calculate_days(birthday)
+    bday = birthday.split('/').join().to_i
+
+    if bday == @today
+      return 0
+    end
+  end
+
+
 end
--- spec/birthday_calculator_spec.rb
+++ spec/birthday_calculator_spec.rb
@@ -2,9 +2,9 @@
 
 require 'birthday_calculator'
 
-describe 'birthday calculator' do
+describe Birthday_calculator do
 
   it "will return 0 if birthday is today" do
-    expect(birthday_calculator("31/03")).to eq 0
+    expect(subject.calculate_days("31/03")).to eq 0
   end
 end
